f90163759370642fc2d51ce151527230ec8303f6
Lenovo <githubsubbu1>
2020-06-03 08:59:16 -0700

    firstcommit

---
+++ authproject/authapp/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import  login_required
+from authapp.forms import SignUpForm
+from django.http import  HttpResponseRedirect
+def home_page_view(request):
+    return  render(request,'authapp/home.html')
+@login_required
+def java_exams_view(request):
+    return render(request,'authapp/javaexams.html')
+@login_required
+def python_exams_view(request):
+    return render(request,'authapp/pythonexams.html')
+
+@login_required
+def aptitude_exams_view(request):
+    return render(request,'authapp/aptitudeexams.html')
+
+def log_out_view(request):
+    return  render(request,'authapp/logout.html')
+
+
+def sign_up_view(request):
+    form=SignUpForm()
+    if request.method=='POST':
+        form=SignUpForm(request.POST)
+        user=form.save()
+        user.set_password(user.password)
+        user.save()
+        return HttpResponseRedirect('/accounts/login')
+    return  render(request,'authapp/signup.html',{'form':form})
